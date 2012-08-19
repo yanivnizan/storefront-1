2ac723bd54de77363c742d8e6cf68eb3fb4ef689
Gur Dotan <gurdotan@gmail.com>
2012-08-19 18:48:05 +0300

    Align the currency store header with the goods header

--- js/views/templates/basic-template.handlebars.js
+++ js/views/templates/basic-template.handlebars.js
@@ -19,13 +19,13 @@ function program1(depth0,data) {
 function program3(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n            <img src=\"";
+  buffer += "\n                <img src=\"";
   foundHelper = helpers.currency;
   stack1 = foundHelper || depth0.currency;
   stack1 = (stack1 === null || stack1 === undefined || stack1 === false ? stack1 : stack1.image);
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "currency.image", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\">\n            ";
+  buffer += escapeExpression(stack1) + "\">\n                ";
   return buffer;}
 
   buffer += "<div id=\"goods-store\" data-role=\"page\">\n    <img src=\"";
@@ -70,12 +70,7 @@ function program3(depth0,data) {
   stack1 = foundHelper || depth0.background;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "background", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"padding\"></div>\n        <div class=\"content\">\n            <h1 class=\"title\">\n                ";
-  foundHelper = helpers.templateTitle;
-  stack1 = foundHelper || depth0.templateTitle;
-  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
-  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "templateTitle", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\n            </h1>\n        <span class=\"balance\">\n            ";
+  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"padding left\">\n            <span class=\"balance\">\n                ";
   foundHelper = helpers.currency;
   stack1 = foundHelper || depth0.currency;
   stack1 = (stack1 === null || stack1 === undefined || stack1 === false ? stack1 : stack1.image);
@@ -86,11 +81,16 @@ function program3(depth0,data) {
   tmp1.inverse = self.noop;
   stack1 = stack2.call(depth0, stack1, tmp1);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n            <label>";
+  buffer += "\n                <label>";
   foundHelper = helpers.currency;
   stack1 = foundHelper || depth0.currency;
   stack1 = (stack1 === null || stack1 === undefined || stack1 === false ? stack1 : stack1.balance);
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "currency.balance", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</label>\n        </span>\n        </div>\n        <div class=\"padding\">\n            <div class=\"back\"></div>\n        </div>\n    </div>\n    <div class=\"items-container\">\n        <ul class=\"items\"></ul>\n    </div>\n</div>\n";
+  buffer += escapeExpression(stack1) + "</label>\n            </span>\n        </div>\n        <div class=\"content\">\n            <h1 class=\"title\">\n                ";
+  foundHelper = helpers.templateTitle;
+  stack1 = foundHelper || depth0.templateTitle;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "templateTitle", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "\n            </h1>\n        </div>\n        <div class=\"padding right\">\n            <div class=\"back\" title=\"Back\"></div>\n        </div>\n    </div>\n    <div class=\"items-container\">\n        <ul class=\"items\"></ul>\n    </div>\n</div>\n";
   return buffer;});
