2391b7fc36851690555bfc4562f40e73d2e65b92
Gur Dotan <gurdotan@gmail.com>
2012-08-27 19:59:19 +0300

    - Add a grunt.js file + package.json for building a distro version of the package - git-ignore node modules

---
+++ grunt.js
@@ -0,0 +1,76 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'YOUR_NAME; Licensed MIT */'
+    },
+//    lint: {
+//      files: ['grunt.js', 'lib/**/*.js', 'spec/**/*.js']
+//    },
+//    qunit: {
+//      files: ['spec/**/*.html']
+//    },
+//    concat: {
+//      dist: {
+//        src: ['<banner:meta.banner>', '<file_strip_banner:lib/FILE_NAME.js>'],
+//        dest: 'dist/FILE_NAME.js'
+//      }
+//    },
+    min: {
+      dist: {
+        src: ['js/store.js', 'js/native-api.js', 'js/models.js'],
+        dest: 'dist/build.js',
+          separator: ';'
+      }
+    },
+//    watch: {
+//      files: '<config:lint.files>',
+//      tasks: 'lint qunit'
+//    },
+//    jshint: {
+//      options: {
+//        curly: true,
+//        eqeqeq: true,
+//        immed: true,
+//        latedef: true,
+//        newcap: true,
+//        noarg: true,
+//        sub: true,
+//        undef: true,
+//        boss: true,
+//        eqnull: true,
+//        browser: true
+//      },
+//      globals: {
+//        jQuery: true
+//      }
+//    },
+
+
+      less: {
+          all: {
+              src: ['css/style.css', 'css/store.less'],
+              dest: 'dist/css/store.css',
+              options: {
+                  compress: true
+              }
+          }
+      },
+
+      uglify: {}
+  });
+
+    grunt.loadNpmTasks('grunt-less');
+
+  // Default task.
+//  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('default', 'min');
+
+};
