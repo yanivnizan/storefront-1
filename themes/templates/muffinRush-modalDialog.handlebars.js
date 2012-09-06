53ca9e531986a7309c7ea1ab9d9fb57d8ddd153c
Gur Dotan <gurdotan@gmail.com>
2012-09-07 00:30:30 +0300

    - Remove button scaling when tapped - Fix balance positioning in currency store

--- themes/templates/muffinRush-modalDialog.handlebars.js
+++ themes/templates/muffinRush-modalDialog.handlebars.js
@@ -1,7 +1,8 @@
+
 Handlebars.templates = Handlebars.templates || {};
 Handlebars.templates['muffinRush-modalDialog'] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-helpers = helpers || Handlebars.helpers;
-var foundHelper, self=this;
+  helpers = helpers || Handlebars.helpers;
+  var foundHelper, self=this;
 
 
-return "<div class=\"modal\">\n    <div class=\"dialog\">\n        <img src=\"img/icons/close-dialog.png\" class=\"close\">\n\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Sorry.png\">\n        <img src=\"examples/muffinRush/modalDialog/Crumbs.png\">\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Question.png\">\n\n        <div class=\"actions\">\n            <img src=\"examples/muffinRush/modalDialog/No.png\" class=\"cancel\">\n            <img src=\"examples/muffinRush/modalDialog/Yes.png\" class=\"buy-more\">\n        </div>\n    </div>\n</div>\n";});
+  return "<div class=\"modal\">\n    <div class=\"dialog\">\n        <img src=\"img/icons/close-dialog.png\" class=\"close\">\n\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Sorry.png\">\n        <img src=\"examples/muffinRush/modalDialog/Crumbs.png\">\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Question.png\">\n\n        <div class=\"actions\">\n            <img src=\"examples/muffinRush/modalDialog/No.png\" class=\"cancel\">\n            <img src=\"examples/muffinRush/modalDialog/Yes.png\" class=\"buy-more\">\n        </div>\n    </div>\n</div>\n";});
--- themes/templates/muffinRush-template.handlebars.js
+++ themes/templates/muffinRush-template.handlebars.js
@@ -17,11 +17,11 @@ function program1(depth0,data) {
 function program3(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n            <div class=\"container\">\n                <label>";
+  buffer += "\n                <div class=\"container\">\n                    <label>";
   stack1 = depth0.balance;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "this.balance", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</label>\n            </div>\n            ";
+  buffer += escapeExpression(stack1) + "</label>\n                </div>\n                ";
   return buffer;}
 
   buffer += "<div id=\"goods-store\" data-role=\"page\">\n    <img src=\"";
@@ -49,7 +49,7 @@ function program3(depth0,data) {
   stack1 = foundHelper || depth0.background;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "background", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance\">\n            <img src=\"examples/muffinRush/YourMuf.png\">\n            ";
+  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance-container\">\n            <div class=\"balance\">\n                <img src=\"examples/muffinRush/YourMuf.png\">\n                ";
   foundHelper = helpers.virtualCurrencies;
   stack1 = foundHelper || depth0.virtualCurrencies;
   stack2 = helpers.each;
@@ -59,52 +59,5 @@ function program3(depth0,data) {
   tmp1.inverse = self.noop;
   stack1 = stack2.call(depth0, stack1, tmp1);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"back\">\n        </div>\n    </div>\n\n</div>\n";
+  buffer += "\n            </div>\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"back\">\n        </div>\n    </div>\n\n</div>\n";
   return buffer;});
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['muffinRush-template'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  var buffer = "", stack1, foundHelper, functionType="function", escapeExpression=this.escapeExpression, self=this;
-
-function program1(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n                <div class=\"container\">\n                    <label>";
-  stack1 = depth0.balance;
-  stack1 = typeof stack1 === functionType ? stack1() : stack1;
-  buffer += escapeExpression(stack1) + "</label>\n                </div>\n                ";
-  return buffer;}
-
-function program3(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n            <div class=\"container\">\n                <label>";
-  stack1 = depth0.balance;
-  stack1 = typeof stack1 === functionType ? stack1() : stack1;
-  buffer += escapeExpression(stack1) + "</label>\n            </div>\n            ";
-  return buffer;}
-
-  buffer += "<div id=\"goods-store\" data-role=\"page\">\n    <img src=\"";
-  foundHelper = helpers.background;
-  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
-  else { stack1 = depth0.background; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance-container\">\n            <div class=\"balance\">\n                <img src=\"examples/muffinRush/YourMuf.png\">\n                ";
-  stack1 = depth0.virtualCurrencies;
-  stack1 = helpers.each.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n            </div>\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"leave-store\">\n        </div>\n        <div>\n            <img src=\"";
-  foundHelper = helpers.moreCurrencyImage;
-  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
-  else { stack1 = depth0.moreCurrencyImage; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "\" class=\"currency buy-more\">\n        </div>\n    </div>\n</div>\n<div id=\"currency-store\" data-role=\"page\">\n    <img src=\"";
-  foundHelper = helpers.background;
-  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
-  else { stack1 = depth0.background; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance\">\n            <img src=\"examples/muffinRush/YourMuf.png\">\n            ";
-  stack1 = depth0.virtualCurrencies;
-  stack1 = helpers.each.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(3, program3, data)});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"back\">\n        </div>\n    </div>\n\n</div>\n";
-  return buffer;});
-})();
