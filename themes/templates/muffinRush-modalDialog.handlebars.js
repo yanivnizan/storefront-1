a713e575d8a7d0f937a4ab7e6cdfe269cb3adc22
refaelos <refaeldakar@gmail.com>
2012-09-06 22:16:48 +0300

    added missing file

---
+++ themes/templates/muffinRush-modalDialog.handlebars.js
@@ -0,0 +1,9 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['muffinRush-modalDialog'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  
+
+
+  return "<div class=\"modal\">\n    <div class=\"dialog\">\n        <img src=\"img/icons/close-dialog.png\" class=\"close\">\n\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Sorry.png\">\n        <img src=\"examples/muffinRush/modalDialog/Crumbs.png\">\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Question.png\">\n\n        <div class=\"actions\">\n            <img src=\"examples/muffinRush/modalDialog/No.png\" class=\"cancel\">\n            <img src=\"examples/muffinRush/modalDialog/Yes.png\" class=\"buy-more\">\n        </div>\n    </div>\n</div>\n";});
+})();
