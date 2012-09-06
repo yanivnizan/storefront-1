1277e6cb0b4c592f9284dd13f694b34d7bb33a96
Gur Dotan <gurdotan@gmail.com>
2012-09-06 18:23:37 +0300

    Make sure the balance is tightly positioned inside the balance container so that variable viewports are supported

--- themes/templates/muffinRush-template.handlebars.js
+++ themes/templates/muffinRush-template.handlebars.js
@@ -7,11 +7,11 @@ Handlebars.templates['muffinRush-template'] = Handlebars.template(function (Hand
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n            <div class=\"container\">\n                <label>";
+  buffer += "\n                <div class=\"container\">\n                    <label>";
   stack1 = depth0.balance;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "this.balance", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</label>\n            </div>\n            ";
+  buffer += escapeExpression(stack1) + "</label>\n                </div>\n                ";
   return buffer;}
 
 function program3(depth0,data) {
@@ -29,7 +29,7 @@ function program3(depth0,data) {
   stack1 = foundHelper || depth0.background;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "background", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance\">\n            <img src=\"examples/muffinRush/YourMuf.png\">\n            ";
+  buffer += escapeExpression(stack1) + "\" class=\"background\">\n    <div class=\"header\">\n        <div class=\"title\">\n            <img src=\"examples/muffinRush/Store_Store.png\">\n        </div>\n        <div class=\"balance-container\">\n            <div class=\"balance\">\n                <img src=\"examples/muffinRush/YourMuf.png\">\n                ";
   foundHelper = helpers.virtualCurrencies;
   stack1 = foundHelper || depth0.virtualCurrencies;
   stack2 = helpers.each;
@@ -39,7 +39,7 @@ function program3(depth0,data) {
   tmp1.inverse = self.noop;
   stack1 = stack2.call(depth0, stack1, tmp1);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"leave-store\">\n        </div>\n        <div>\n            <img src=\"";
+  buffer += "\n            </div>\n        </div>\n    </div>\n    <div class=\"items-container\">\n    </div>\n    <div class=\"footer\">\n        <div>\n            <img src=\"examples/muffinRush/back.png\" class=\"leave-store\">\n        </div>\n        <div>\n            <img src=\"";
   foundHelper = helpers.moreCurrencyImage;
   stack1 = foundHelper || depth0.moreCurrencyImage;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
