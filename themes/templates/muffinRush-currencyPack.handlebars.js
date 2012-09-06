49ad3649b611def024921280b22ce49543de3100
Gur Dotan <gurdotan@gmail.com>
2012-09-06 17:05:08 +0300

    Style the currency packs - vertical alignment and font size

--- themes/templates/muffinRush-currencyPack.handlebars.js
+++ themes/templates/muffinRush-currencyPack.handlebars.js
@@ -25,5 +25,5 @@ Handlebars.templates['muffinRush-currencyPack'] = Handlebars.template(function (
   stack1 = foundHelper || depth0.price;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "price", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</label>\n</div>\n";
+  buffer += escapeExpression(stack1) + "$</label>\n</div>\n";
   return buffer;});
