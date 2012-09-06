103fa62afec49005ae914bd251a5bbfad71deb81
Gur Dotan <gurdotan@gmail.com>
2012-09-06 18:50:39 +0300

    Missing from last commit

--- themes/templates/muffinRush-modalDialog.handlebars.js
+++ themes/templates/muffinRush-modalDialog.handlebars.js
@@ -1,9 +1,7 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['muffinRush-modalDialog'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  
+Handlebars.templates = Handlebars.templates || {};
+Handlebars.templates['muffinRush-modalDialog'] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+helpers = helpers || Handlebars.helpers;
+var foundHelper, self=this;
 
 
-  return "<div class=\"modal\">\n    <div class=\"dialog\">\n        <img src=\"img/icons/close-dialog.png\" class=\"close\">\n\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Sorry.png\">\n        <img src=\"examples/muffinRush/modalDialog/Crumbs.png\">\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Question.png\">\n\n        <div class=\"actions\">\n            <img src=\"examples/muffinRush/modalDialog/No.png\" class=\"cancel\">\n            <img src=\"examples/muffinRush/modalDialog/Yes.png\" class=\"buy-more\">\n        </div>\n    </div>\n</div>\n";});
-})();
+return "<div class=\"modal\">\n    <div class=\"dialog\">\n        <img src=\"img/icons/close-dialog.png\" class=\"close\">\n\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Sorry.png\">\n        <img src=\"examples/muffinRush/modalDialog/Crumbs.png\">\n        <img src=\"examples/muffinRush/modalDialog/AlertText_Question.png\">\n\n        <div class=\"actions\">\n            <img src=\"examples/muffinRush/modalDialog/No.png\" class=\"cancel\">\n            <img src=\"examples/muffinRush/modalDialog/Yes.png\" class=\"buy-more\">\n        </div>\n    </div>\n</div>\n";});
